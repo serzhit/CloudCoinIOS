e99b096449228a0a97afd09ad542d659f6f3bd71
Sergey Gitinsky <sergey@gitinsky.com>
2017-05-21 01:23:40 +0300

    * CloudCoinIOS.userprefs: Help text changed. SSPCL-21, SSPCL-22
    * Main.storyboard:
    * NewPassViewController.designer.cs:

--- CloudCoinIOS/CloudCoinIOS/ViewControllers/NewPassViewController.designer.cs
+++ CloudCoinIOS/CloudCoinIOS/ViewControllers/NewPassViewController.designer.cs
@@ -1,66 +1,72 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace CloudCoinIOS
 {
-	[Register ("NewPassViewController")]
-	partial class NewPassViewController
-	{
-		[Outlet]
-		UIKit.UIButton btnCancel { get; set; }
+    [Register ("NewPassViewController")]
+    partial class NewPassViewController
+    {
+        [Outlet]
+        UIKit.UIButton btnCancel { get; set; }
 
-		[Outlet]
-		UIKit.UIButton btnOk { get; set; }
 
-		[Outlet]
-		UIKit.NSLayoutConstraint textConstraintHeight { get; set; }
+        [Outlet]
+        UIKit.UIButton btnOk { get; set; }
 
-		[Outlet]
-		UIKit.UITextField txtCfrPassword { get; set; }
 
-		[Outlet]
-		UIKit.UITextView txtExplain { get; set; }
+        [Outlet]
+        UIKit.NSLayoutConstraint textConstraintHeight { get; set; }
 
-		[Outlet]
-		UIKit.UITextField txtPassword { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (btnOk != null) {
-				btnOk.Dispose ();
-				btnOk = null;
-			}
 
-			if (txtCfrPassword != null) {
-				txtCfrPassword.Dispose ();
-				txtCfrPassword = null;
-			}
+        [Outlet]
+        UIKit.UITextField txtCfrPassword { get; set; }
 
-			if (txtPassword != null) {
-				txtPassword.Dispose ();
-				txtPassword = null;
-			}
 
-			if (btnCancel != null) {
-				btnCancel.Dispose ();
-				btnCancel = null;
-			}
+        [Outlet]
+        UIKit.UITextView txtExplain { get; set; }
 
-			if (textConstraintHeight != null) {
-				textConstraintHeight.Dispose ();
-				textConstraintHeight = null;
-			}
 
-			if (txtExplain != null) {
-				txtExplain.Dispose ();
-				txtExplain = null;
-			}
-		}
-	}
+        [Outlet]
+        UIKit.UITextField txtPassword { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCancel != null) {
+                btnCancel.Dispose ();
+                btnCancel = null;
+            }
+
+            if (btnOk != null) {
+                btnOk.Dispose ();
+                btnOk = null;
+            }
+
+            if (textConstraintHeight != null) {
+                textConstraintHeight.Dispose ();
+                textConstraintHeight = null;
+            }
+
+            if (txtCfrPassword != null) {
+                txtCfrPassword.Dispose ();
+                txtCfrPassword = null;
+            }
+
+            if (txtExplain != null) {
+                txtExplain.Dispose ();
+                txtExplain = null;
+            }
+
+            if (txtPassword != null) {
+                txtPassword.Dispose ();
+                txtPassword = null;
+            }
+        }
+    }
 }
